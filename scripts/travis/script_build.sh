8f6ab9ec68a6e67ac3a49ebf52569369bacd571a
Hannes Janetzek <hannes.janetzek@gmail.com>
2017-05-10 12:09:03 +0200

    use OSMesa package on travis

--- scripts/travis/script_build.sh
+++ scripts/travis/script_build.sh
@@ -12,7 +12,7 @@ fi
 if [[ ${PLATFORM} == "linux" ]]; then
     # Build linux project
     echo "Building linux project"
-    CMAKE_OPTIONS="-DUNIT_TESTS=1 -DBENCHMARK=1" make -j 4 linux
+    CMAKE_OPTIONS="-DHEADLESS=1 -DUSE_SYSTEM_OSMESA_LIBS=1 -DUNIT_TESTS=1 -DBENCHMARK=1" make -j 4 linux
 fi
 
 if [[ ${PLATFORM} == "ios" ]]; then
